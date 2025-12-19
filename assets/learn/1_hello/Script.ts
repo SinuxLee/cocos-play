2638209312fd2ecff940e3006371b846e6ce10dd
sinuxlee <pingfan14@gmail.com>
2025-12-19 14:36:26 +0800

    more comments for simple effect

--- assets/learn/1_hello/Script.ts
+++ assets/learn/1_hello/Script.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Sprite, Vec4 } from 'cc';
+import { _decorator, Component, Sprite, Vec4, renderer } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Script')
@@ -7,31 +7,40 @@ export class Script extends Component {
     sprite: Sprite | null = null;
 
     private _time = 0;
+    private _mat: renderer.MaterialInstance | null = null;
 
     start() {
         // 如果没在 Inspector 手动拖拽，就尝试自动获取同节点上的 Sprite
         if (!this.sprite) {
             this.sprite = this.getComponent(Sprite);
         }
+
+        // 一定要改实例，而不是共享的材质信息
+        if (!this._mat) {
+            this._mat = this.sprite.getMaterialInstance(0);
+        }
     }
 
-    update(dt: number) {
-        if (!this.sprite) return;
-        
-        // 一定要改实例，而不是共享的材质信息
-        const mat = this.sprite.getMaterialInstance(0);
-        if (!mat) return;
+    // update(dt: number) {
+    //     if (!this.sprite || !this._mat) return;
+    //     this._time += dt;
 
+    //     // 读出旧 params，改 y，再写回（别把 x/z/w 覆盖掉）
+    //     const p = this._mat.getProperty('params') as Vec4;
+    //     if (p === null) {
+    //         this._mat.setProperty('params', new Vec4(1, this._time, 1, 0.2));
+    //         return
+    //     }
+
+    //     p.y = this._time;
+    //     this._mat.setProperty('params', p);
+    // }
+
+
+    update(dt: number) {
+        if (!this.sprite || !this._mat) return;
         this._time += dt;
 
-        // 读出旧 params，改 y，再写回（别把 x/z/w 覆盖掉）
-        const p = mat.getProperty('params') as Vec4;
-        if (p === null) {
-            mat.setProperty('params', new Vec4(1, this._time, 1, 0.2));
-            return
-        }
-
-        p.y = this._time;
-        mat.setProperty('params', p);
+        this._mat.setProperty('time', this._time);
     }
 }
